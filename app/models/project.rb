cf8da81eafcc483bb50832c26f7a4a3c40348e78
Hiroyuki Sano <sh19910711@gmail.com>
2016-08-09 05:54:33 +0000

    experiment: send request from project

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,6 +10,44 @@ class Project < ApplicationRecord
     FileUtils.rm_rf dirpath
   end
 
+  if Rails.env.development?
+    require 'rubygems/package'
+    require 'httpclient'
+
+    def __send_request(request_method, request_url, request_params)
+      c = ::HTTPClient.new
+      c.send(http_client_method(request_method), request_url, { file: tar_io }.merge(request_params))
+    end
+
+    private
+
+      def tar_io
+        io = ::StringIO.new
+        io.class.class_eval { attr_accessor :path }
+        io.path = "file.tar"
+
+        ::Gem::Package::TarWriter.new(io) do |w|
+          entities.each do |e|
+            w.add_file(e.path, e.stat.mode) { |f| f.write(e.readfile) }
+          end
+        end
+
+        io.rewind
+        io
+      end
+
+      def http_client_method(request_method)
+        case request_method
+        when 'POST'
+          :post_content
+        when 'PUT'
+          :put_content
+        else
+          raise 'unknown method'
+        end
+      end
+  end
+
   private
 
     def dirpath
